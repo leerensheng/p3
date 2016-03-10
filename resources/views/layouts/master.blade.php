afd979755e0e2d5ab502d63f9976ce6503021d5d
Matt Lee <matthewbrentlee@g.harvard.edu>
2016-03-10 16:23:27 +0800

    Added CSS file, blade files, badcow lorem ipsum and fzaninotto faker

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+    <title>
+    </title>
+    <meta charset='utf-8'>
+    <link href="/css/p3.css" type='type/css' rel='stylesheet'>
+</head>
+
+<body>
+    <header>
+    </header>
+
+    <section>
+    </section>
+
+    <footer>
+    </footer>
+</body>
+</html>
