7aecc47f8f894deb459c5acccedc93ec96304318
Matt Lee <matthewbrentlee@g.harvard.edu>
2016-03-12 19:07:08 +0800

    Added routes

--- app/Http/Controllers/GeneratorController.php
+++ app/Http/Controllers/GeneratorController.php
@@ -1,9 +1,17 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace p3\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+use p3\Http\Controllers\Controller;
 
 class GeneratorController extends Controller {
+
+    public function getLorem() {
+        return 'This is the lorem ipsum page';    
+    }
+
+    public function getUsers() {
+        return 'This is the get users page';
+    }
 
 } #eoc
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,8 +2,13 @@
 
 Route::group(['middleware' => ['web']], function () {
 
+    #Route::get('/', 'getIndex@GeneratorController');
     Route::get('/', function () {
-        return view('welcome');
+        return 'This is where you can choose to generate lorum ipsum or random users';
     });
+
+    Route::get('/loremipsum', 'GeneratorController@getLorem');
+
+    Route::get('/randomuser', 'GeneratorController@getUsers');
 
 });
